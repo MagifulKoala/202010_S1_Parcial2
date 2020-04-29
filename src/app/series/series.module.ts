3bb1e561c0650d10cc8b8999477cd9e04e85aba1
Pablo Dorado <p.dorado@uniandes.edu.co>
2020-04-29 08:56:14 -0500

    updates

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SeriesComponent } from './series.component';
+
+
+import {SeriesListarComponent} from './series-listar/series-listar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SeriesComponent]
+  declarations: [SeriesListarComponent],
+  exports: [SeriesListarComponent]
 })
+
 export class SeriesModule { }
--- src/app/series/series.ts
+++ src/app/series/series.ts
@@ -1,2 +1,16 @@
 export class Series {
+
+
+  constructor(private nameA: string, private channelA: string, private seasonsA: number,
+              private descriptionA: string, private webpageA: string, private posterA: string,) {
+
+               }
+
+  get name(): string { return this.nameA; }
+  get channel(): string { return this.channelA; }
+  get seasons(): number { return this.seasonsA; }
+  get description(): string { return this.descriptionA; }
+  get webpage(): string { return this.webpageA; }
+  get poster(): string { return this.posterA ; }
+
 }
