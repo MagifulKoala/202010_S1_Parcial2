0a0669d0f61ab456dff63d7b573473a29dda59a3
Pablo Dorado <p.dorado@uniandes.edu.co>
2020-04-29 09:30:30 -0500

    updates

--- src/app/series/series.service.ts
+++ src/app/series/series.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+import { Series } from './series';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
 
-constructor() { }
+  private apiUrl = environment.baseUrl ;
+
+constructor(private http: HttpClient) { }
+
+getSeries(): Observable<Series[]> {
+  return this.http.get<Series[]>(this.apiUrl);
+}
 
 }
