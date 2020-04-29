ca59991d643a5677f27cfe570131666b8441dbdf
Pablo Dorado <p.dorado@uniandes.edu.co>
2020-04-29 08:17:36 -0500

    primera parte punto 1

---
+++ src/app/series/series-listar/series-listar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-series-listar',
+  templateUrl: './series-listar.component.html',
+  styleUrls: ['./series-listar.component.css']
+})
+export class SeriesListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
